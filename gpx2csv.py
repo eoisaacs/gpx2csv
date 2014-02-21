4c165618cdc31178a25cc9ce1a47d5d47d0a9ad2
Eric Isaacs <eric@eoisaacs.com>
2014-02-20 21:29:14 -0500

    Script first commit

---
+++ gpx2csv.py
@@ -0,0 +1,39 @@
+#usage:
+#python3 gpx2csv.py outputDirectory
+#remember to escape spaces in file paths with \
+
+#load required modules
+import sys
+import os
+import xml.etree.ElementTree as ET
+import csv
+
+#get the full path of the source, parent directory of source, list of files in source, and remove .DS_Store file from list
+directory = os.path.abspath(sys.argv[1])
+directoryParent = os.path.split(directory)[0]
+csvDir = os.path.join(directoryParent,'csv')
+fileNames = os.listdir(directory)
+if '.DS_Store' in fileNames:
+	fileNames.remove('.DS_Store')
+filePaths = [os.path.join(directory,fn) for fn in fileNames]
+
+#check if csv folder exists in same parent directory as source, if not make it
+if not os.path.exists(csvDir):
+    os.makedirs(csvDir)
+
+#parse gpx files for desired data
+for x in range(0, len(fileNames)):
+	tree = ET.parse(filePaths[x])
+	root = tree.getroot()
+	time = tree.findall('.//{http://www.topografix.com/GPX/1/1}time')
+	trackPoints = tree.findall('.//{http://www.topografix.com/GPX/1/1}trkpt')
+	elevation = tree.findall('.//{http://www.topografix.com/GPX/1/1}ele')
+	hr = tree.findall('.//{http://www.garmin.com/xmlschemas/TrackPointExtension/v1}hr')
+
+#write csv file for each gpx with heading and data sorted by column
+	targetPath = os.path.join(csvDir,fileNames[x])
+	with open(targetPath + '.csv', 'w') as csvfile:
+		datawriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		datawriter.writerow(['time', 'lat', 'lon', 'elevation', 'hr'])
+		for x in range(0, len(trackPoints)):
+			datawriter.writerow([time[x].text, trackPoints[x].attrib['lat'], trackPoints[x].attrib['lon'], elevation[x].text, hr[x].text])
